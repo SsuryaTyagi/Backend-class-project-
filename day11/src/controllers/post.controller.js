527d57eb346556f0912bf169bbe7382ef2c7abf8
SsuryaTyagi <2040surya@gmail.com>
2026-02-16 02:00:50 +0530

    one

--- day11/src/controllers/post.controller.js
+++ day11/src/controllers/post.controller.js
@@ -1,9 +1,30 @@
-const postModel = require("../models/post.models")
+require("dotenv").config();
+const postModel = require("../models/post.models");
+const ImageKit = require("@imagekit/nodejs");
 
+const client = new ImageKit({
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY, // This is the default and can be omitted
+});
 
-const postController = async (req,res)=>{
-    console.log(req.body);
-    
-}
+const postController = async (req, res) => {
+  try {
+    console.log(req.body, req.file);
+
+    const response = await client.files.upload({
+      file: req.file.buffer.toString("base64"),
+      fileName: req.file.originalname,
+    });
+
+    res.status(201).json({
+      message: "File uploaded successfully",
+      data: response,
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Upload failed" });
+  }
+};
+
 
 module.exports = postController;
--- day11/src/routes/post.routes.js
+++ day11/src/routes/post.routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const postController = require("../controllers/post.controller")
+const multer = require("multer")
+const upload = multer({ storage: multer.memoryStorage() })
 
 const postRouter = express.Router();
 
 
 
-postRouter.post("/",postController);
+postRouter.post("/", upload.single('image'),postController);
 
 
 
