10b39ad5ea96e923d8fc793589ee5627e4cdca7b
SsuryaTyagi <2040surya@gmail.com>
2026-05-06 20:21:13 +0530

    Perplexity

---
+++ Perplexity/backend/src/config/database.js
@@ -0,0 +1,13 @@
+require("dotenv").config()
+const mongoose = require("mongoose");
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGOOSE);
+    console.log('Connected to MongoDB!');
+  } catch (err) {
+    console.error('Connection error:', err);
+  }
+}
+
+module.exports = connectDB
